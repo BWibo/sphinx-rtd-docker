2038b8d261aae4e0d1818bb9afee383148eb6ec3
Bruno Willenborg <b.willenborg@tum.de>
2021-04-16 11:58:23 +0200

    build multiple versions and latest tag

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 \
-docker build -t bwibo/sphinx-rtd --build-arg BASEIMAGE_TAG=3.5.2 .
+# List Sphinx versions here
+versions=("3.5.2" "3.5.3" "3.5.4")
+
+for i in "${versions[@]}"; do
+  DOCKER_BUILDKIT=1 \
+    docker build -t "bwibo/sphinx-rtd:$i" --build-arg BASEIMAGE_TAG="$i" .
+
+  docker push "bwibo/sphinx-rtd:$i"
+
+  # latest tag
+  if [[ "$i" == "${versions[-1]}" ]]; then
+    docker tag "bwibo/sphinx-rtd:$i" "bwibo/sphinx-rtd:latest"
+    docker push "bwibo/sphinx-rtd:latest"
+  fi
+done
