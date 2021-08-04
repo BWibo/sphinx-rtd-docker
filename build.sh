15e61e9ec44863fd592ecd90471800905d3491e0
Bruno Willenborg <b.willenborg@tum.de>
2021-08-04 15:01:47 +0200

    fixed version array

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # List Sphinx versions here
-versions=("3.5.2" "3.5.3" "3.5.4" "4.0.0" "4.0.1" "4.0.2" "4.0.3", "4.1.0", "4.1.1", "4.1.2")
+versions=("3.5.2" "3.5.3" "3.5.4" "4.0.0" "4.0.1" "4.0.2" "4.0.3" "4.1.0" "4.1.1" "4.1.2")
 
 for i in "${versions[@]}"; do
   DOCKER_BUILDKIT=1 \
