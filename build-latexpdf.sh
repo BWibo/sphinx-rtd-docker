a868fa25aa9662b31ad67898e16e93ebf2ba58a6
Bruno Willenborg <b.willenborg@tum.de>
2021-09-14 09:50:26 +0200

    use latexpdf Dockerfile

--- build-latexpdf.sh
+++ build-latexpdf.sh
@@ -5,7 +5,10 @@ versions=("3.5.2" "3.5.3" "3.5.4" "4.0.0" "4.0.1" "4.0.2" "4.0.3" "4.1.0" "4.1.1
 
 for i in "${versions[@]}"; do
   DOCKER_BUILDKIT=1 \
-    docker build -t "bwibo/sphinx-rtd-latexpdf:$i" --build-arg BASEIMAGE_TAG="$i" .
+    docker build . \
+    -f Dockerfile.latexpdf \
+    -t "bwibo/sphinx-rtd-latexpdf:$i" \
+    --build-arg BASEIMAGE_TAG="$i"
 
   docker push "bwibo/sphinx-rtd-latexpdf:$i"
 
